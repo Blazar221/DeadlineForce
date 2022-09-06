39a5b48b359d65518e93f76d588b39e09c5b0621
sunyw99 <sunyw99@163.com>
2022-09-05 17:30:51 -0700

    First commit, 2 walls jump game prototype and .gitignore

---
+++ Assets/Script/ColliderGO.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class ColliderGO : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    public void CallPlayerMethod()
+    {
+        Debug.Log(PlayerTransform.name);
+        PlayerTransform.GetComponent<Player>().CallThisMethod();
+    }
+}
