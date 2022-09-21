703190c44aec45ec8b2e956026e052cec5f88027
Haorui Chen <44103608+Original-Thunderbird@users.noreply.github.com>
2022-09-21 04:50:48 -0700

    Spawn all objects with a uniform spawner

--- Assets/Script/GravSwitch.cs
+++ Assets/Script/GravSwitch.cs
@@ -14,7 +14,7 @@ public class GravSwitch : MonoBehaviour
     void Start()
     {
         pos = transform.position;
-        Destroy(gameObject, 3f);
+        Destroy(gameObject, 1.5f);
     }
     
     // Update is called once per frame
