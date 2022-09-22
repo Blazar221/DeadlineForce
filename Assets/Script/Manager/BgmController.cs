4043f5926b63f178d5ebb7ff248f367bbc437eaa
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2022-09-21 22:23:53 -0700

    Revert "Merge branch 'main' of https://github.com/Blazar221/DeadlineForce"
    This reverts commit 4925fa97a3d80a15b5c5690d4e635d70d37cc8a5, reversing
    changes made to a7fe8fa78a0c85fb27f090c65b75ac1631228059.

--- Assets/Script/Manager/BgmController.cs
+++ Assets/Script/Manager/BgmController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BgmManager : MonoBehaviour
+public class BgmController : MonoBehaviour
 {
     [SerializeField]
     AudioSource audioSource;
