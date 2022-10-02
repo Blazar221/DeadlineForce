015bcceef498d0da26ce5fd5e67b1f1399b64d41
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2022-10-01 18:48:16 -0700

    Update PauseMenu.cs

--- Assets/Script/Menu/PauseMenu.cs
+++ Assets/Script/Menu/PauseMenu.cs
@@ -12,16 +12,13 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input. GetKeyDown(KeyCode.P))
+        if (Input. GetKeyDown(KeyCode.Escape) && isGamePaused == false)
         {
-            if (isGamePaused)
-            {
-                ResumeGame();
-            }
-            else
-            {
+            // if (isGamePaused)
+            // {
+            //     ResumeGame();
+            // }
                 PauseGame();
-            }
         }
     }
 
