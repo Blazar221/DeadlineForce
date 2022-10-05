877faea0cb570249cc8b1d31b026403f36c29f1b
Youqi Rong <rongyouqi@gmail.com>
2022-10-04 20:42:14 -0700

    L2Spawner

--- Assets/Script/Spawner/L2Spawner.cs
+++ Assets/Script/Spawner/L2Spawner.cs
@@ -28,7 +28,7 @@ public class L2Spawner : MonoBehaviour
     private Object[] objArr = new Object[]
     {
         // best lane
-        new Object(new float[]{0f, 0f}, 0, 1),
+        new Object(new float[]{0f, 0f}, 0, 1, true), // index zero doesn't spawn
         new Object(new float[]{0.545f, 0.545f}, 2, 1, true),
         new Object(new float[]{0.818f, 0.818f}, 2, 3, true),
         new Object(new float[]{1.091f, 1.091f}, 2, 1, true),
@@ -80,7 +80,7 @@ public class L2Spawner : MonoBehaviour
         new Object(new float[]{16.636f, 16.636f}, 2, 1, true),
         new Object(new float[]{16.909f, 16.909f}, 2, 1, true),
         new Object(new float[]{17.182f, 17.182f}, 2, 1, true),
-        new Object(new float[]{17.455f, 17.455f}, 2, 0, true),
+        new Object(new float[]{17.455f, 17.455f}, 2, 0, true), // gravity switch
         new Object(new float[]{18.545f, 18.545f}, 1, 3, true),
         new Object(new float[]{18.818f, 18.818f}, 1, 1, true),
         new Object(new float[]{19.091f, 19.091f}, 1, 3, true),
@@ -141,7 +141,7 @@ public class L2Spawner : MonoBehaviour
         new Object(new float[]{34.091f, 34.091f}, 1, 1, true),
         new Object(new float[]{34.364f, 34.364f}, 1, 1, true),
         new Object(new float[]{34.636f, 34.636f}, 1, 1, true),
-        new Object(new float[]{34.909f, 34.909f}, 1, 0, true),
+        new Object(new float[]{34.909f, 34.909f}, 1, 0, true), // gravity switch
         new Object(new float[]{35.455f, 35.455f}, 3, 1, true),
         new Object(new float[]{35.727f, 35.727f}, 3, 1, true),
         new Object(new float[]{36.0f, 36.0f}, 3, 1, true),
@@ -193,7 +193,7 @@ public class L2Spawner : MonoBehaviour
         new Object(new float[]{51.545f, 51.545f}, 3, 1, true),
         new Object(new float[]{51.818f, 51.818f}, 3, 1, true),
         new Object(new float[]{52.091f, 52.091f}, 3, 1, true),
-        new Object(new float[]{52.364f, 52.364f}, 3, 0, true),
+        new Object(new float[]{52.364f, 52.364f}, 3, 0, true), // gravity
         new Object(new float[]{53.454f, 53.454f}, 0, 1, true),
         new Object(new float[]{53.727f, 53.727f}, 0, 1, true),
         new Object(new float[]{54.0f, 54.0f}, 0, 1, true),
