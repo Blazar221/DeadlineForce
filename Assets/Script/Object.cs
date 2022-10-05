1365d86f2092c1d55439419b04c8cf1b7037c6f9
Eric_Guo <yufeiguo@usc.edu>
2022-10-04 20:05:58 -0700

    update level3 main lane

--- Assets/Script/Object.cs
+++ Assets/Script/Object.cs
@@ -4,12 +4,14 @@
     {
         private float[] timeStamp;
         private int type, pos;
+        private bool isMain;
 
-        public Object(float[] timeStamp, int pos, int type)
+        public Object(float[] timeStamp, int pos, int type, bool isMain)
         {
             this.timeStamp = timeStamp;
             this.type = type;
             this.pos = pos;
+            this.isMain = isMain;
         }
 
         public float[] TimeStamp
@@ -25,6 +27,11 @@
         public int Pos
         {
             get { return pos; }
+        }
+
+        public bool IsMain
+        {
+            get { return isMain; }
         }
     }
 }
