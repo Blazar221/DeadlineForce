ed8df77fd731d99f608205c835a3cb5e6e1f29e9
Haorui Chen <44103608+Original-Thunderbird@users.noreply.github.com>
2022-10-07 15:09:22 -0700

    lvl2 w/ fixed update

--- Assets/Script/Spawner/tempL2Spawner.cs
+++ Assets/Script/Spawner/tempL2Spawner.cs
@@ -7,6 +7,7 @@ using Object = Script.Object;
 
 public class tempL2Spawner : MonoBehaviour
 {
+    [SerializeField] private GameObject platform;
     [SerializeField] private GameObject gravSwitch;
     [SerializeField] private GameObject note;
     [SerializeField] private GameObject longNote;
@@ -14,7 +15,7 @@ public class tempL2Spawner : MonoBehaviour
     [SerializeField] private GameObject block;
     [SerializeField] private GameObject bgm;
 
-    private GameObject newItem, newMine;
+    private GameObject newItem, newMine, newPlatform;
     private Vector3 spawnPos;
     private Block blockHandler;
     private Note noteHandler;
@@ -438,6 +439,12 @@ public class tempL2Spawner : MonoBehaviour
         new Object(new float[]{69.545f, 69.545f}, 1, 3, false),
         new Object(new float[]{69.818f, 69.818f}, 3, 1, false),
         new Object(new float[]{69.818f, 69.818f}, 1, 1, false),
+        
+        //platform
+        new Object(new float[]{0.0f, 17.45f}, 4, 4, false),
+        new Object(new float[]{18.5f, 34.909f}, 4, 4, false),
+        new Object(new float[]{36.00f, 52.00f}, 4, 4, false),
+        new Object(new float[]{53.50f, 70.00f}, 4, 4, false),
     };
 
     private void Awake()
@@ -461,59 +468,70 @@ public class tempL2Spawner : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log("song position:"+ _bgmHandler.songPosition);
-        Debug.Log("ind:"+ ind);
+        //Debug.Log("song position:"+ _bgmHandler.songPosition);
+        //Debug.Log("ind:"+ ind);
         if (_bgmHandler.started && ind < objArr.Length && objArr[ind].TimeStamp[0] <= _bgmHandler.songPosition + 2)
         {
-            xLen = noteHandler.transform.localScale.x;
+            //do
+            //{
+                xLen = noteHandler.transform.localScale.x;
 
-            float yPos = objArr[ind].Pos switch
-            {
-                0 => 4,
-                1 => 1,
-                2 => -1,
-                3 => -4,
-                _ => 0,
-            };
+                float yPos = objArr[ind].Pos switch
+                {
+                    0 => 4,
+                    1 => 1,
+                    2 => -1,
+                    3 => -4,
+                    _ => 0,
+                };
 
-            if (objArr[ind].TimeStamp[1] - objArr[ind].TimeStamp[0] != 0)
-            {
-                xLen = (objArr[ind].TimeStamp[1] - objArr[ind].TimeStamp[0]) * noteHandler.speed * (1 / Time.fixedDeltaTime);
-            }
+                if (objArr[ind].TimeStamp[1] - objArr[ind].TimeStamp[0] != 0)
+                {
+                    xLen = (objArr[ind].TimeStamp[1] - objArr[ind].TimeStamp[0]) * noteHandler.speed *
+                           (1 / Time.fixedDeltaTime);
+                }
 
-            spawnPos = new Vector3(playerX + noteHandler.speed * (2f / Time.fixedDeltaTime) + xLen / 2, yPos, 0);
+                spawnPos = new Vector3(playerX + noteHandler.speed * (2f / Time.fixedDeltaTime) + xLen / 2, yPos, 0);
 
-            // start spawn
-            switch (objArr[ind].Type)
-            {
-                // gravSwitch
-                case 0:
-                    newItem = Instantiate(gravSwitch, spawnPos, Quaternion.identity);
-                    Destroy(newItem, 3f);
-                    if (objArr[ind].Pos == 1)
-                    {
-                        newItem.transform.localScale = new Vector3(1, -1, 1);
-                    }
-                    break;
-                // short note
-                case 1:
-                    newItem = Instantiate(note, spawnPos, Quaternion.identity);
-                    Destroy(newItem, 3f);
-                    break;
-                // long note
-                case 2:
-                    newItem = Instantiate(longNote, spawnPos, Quaternion.identity);
-                    var newLongNote = newItem.GetComponent<LongNote>();
-                    newLongNote.SetLength(xLen);
-                    Destroy(newItem, 3f/2.46f*xLen);
-                    break;
-                //block
-                case 3:
-                    newItem = Instantiate(block, spawnPos, Quaternion.identity);
-                    Destroy(newItem, 3f);
-                    break;
-            }
-            ind++;
+                // start spawn
+                switch (objArr[ind].Type)
+                {
+                    // gravSwitch
+                    case 0:
+                        newItem = Instantiate(gravSwitch, spawnPos, Quaternion.identity);
+                        Destroy(newItem, 3f);
+                        if (objArr[ind].Pos == 1)
+                        {
+                            newItem.transform.localScale = new Vector3(1, -1, 1);
+                        }
+
+                        break;
+                    // short note
+                    case 1:
+                        newItem = Instantiate(note, spawnPos, Quaternion.identity);
+                        Destroy(newItem, 3f);
+                        break;
+                    // long note
+                    case 2:
+                        newItem = Instantiate(longNote, spawnPos, Quaternion.identity);
+                        var newLongNote = newItem.GetComponent<LongNote>();
+                        newLongNote.SetLength(xLen);
+                        Destroy(newItem, 3f / 2.46f * xLen);
+                        break;
+                    //block
+                    case 3:
+                        newItem = Instantiate(block, spawnPos, Quaternion.identity);
+                        Destroy(newItem, 3f);
+                        break;
+                    case 4:
+                        newPlatform = Instantiate(platform, spawnPos, Quaternion.identity);
+                        var newPlatform_ = newPlatform.GetComponent<platform>();
+                        newPlatform_.SetLength(xLen);
+                        Destroy(newPlatform, 3f / 2.46f * xLen);
+                        break;
+                }
+                ind++;
+            //} while (ind + 1 < objArr.Length && objArr[ind].TimeStamp[0] == objArr[ind + 1].TimeStamp[0]);
         }
         
     }
