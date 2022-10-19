537057c0383510587194e46d344ca8fdff307efa
ChaoyiZh <chaoyizh@gmail.com>
2022-10-19 15:38:08 -0700

    add attacknotification in level 0

---
+++ Assets/Script/GameHint/AttackNotification.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attacknotification : MonoBehaviour
+{
+
+    public float blinkSpeed;
+    private bool isAddAlpha;
+    private float timer;
+    public float timeval = 1;
+
+    private Text t;
+    private void Start()
+    {
+       t  = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (isAddAlpha)
+        {
+            t.color += new Color(0, 0, 0, Time.deltaTime * blinkSpeed);
+            if (timer >= timeval)
+            {
+                t.color = new Color(t.color.r, t.color.g, t.color.b, 1);
+                isAddAlpha = false;
+                timer = 0;
+            }
+        }
+        else
+        {
+            t.color -= new Color(0, 0, 0, Time.deltaTime * blinkSpeed);
+            if (timer >= timeval)
+            {
+                t.color = new Color(t.color.r,t.color.g, t.color.b, 0);
+                isAddAlpha = true;
+                timer = 0;
+            }
+        }
+    }
+
+}
