54626803a8e8da922f3f2d670df4e685765a9f3c
Blazar <805781038@qq.com>
2022-11-02 23:35:59 -0700

    skill: shield UI added

--- Assets/Script/Player/PlayerSkill.cs
+++ Assets/Script/Player/PlayerSkill.cs
@@ -4,36 +4,58 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
+    public static PlayerSkill instance;
+
     [SerializeField]
     private GameObject originPlayer;
     private GameObject copyPlayer;
 
     private PlayerControl originPlayerControl;
     
+    [SerializeField]
+    private GameObject shield;
+    
     private float cloneExistingTime = 7f;
     private bool hasClone = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    private void Awake() {
+        instance = this;
         originPlayerControl = originPlayer.GetComponent<PlayerControl>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.X)){
-            int yPos = 3 - originPlayerControl.GetYPos();
-            
-            Vector3 spawnPos = originPlayer.transform.position;
-            spawnPos.y = originPlayerControl.playerYPosArr[yPos];
-            copyPlayer = Instantiate(originPlayer, spawnPos, Quaternion.identity);
-
-            Destroy(copyPlayer, cloneExistingTime);
-            
-            PlayerControl copyPlayerHandler = copyPlayer.GetComponent<PlayerControl>();
-            copyPlayerHandler.EnableClone();
-            copyPlayerHandler.SetYPos(yPos);
+            CallShieldSkill();
         }
+    }
+
+    void CallShieldSkill()
+    {
+        shield.SetActive(true);
+        StartCoroutine(CloseShieldSkill());
+    }
+
+    IEnumerator CloseShieldSkill()
+    {
+        yield return new WaitForSeconds(3f);
+        shield.SetActive(false);
+    }
+
+    void CallCloneSkill()
+    {
+        int yPos = 3 - originPlayerControl.GetYPos();
+        
+        Vector3 spawnPos = originPlayer.transform.position;
+        spawnPos.y = originPlayerControl.playerYPosArr[yPos];
+        copyPlayer = Instantiate(originPlayer, spawnPos, Quaternion.identity);
+        // Clone should disappear after a while
+        Destroy(copyPlayer, cloneExistingTime);
+        // Make clone mirror the origin
+        PlayerControl copyPlayerHandler = copyPlayer.GetComponent<PlayerControl>();
+        copyPlayerHandler.EnableClone();
+        copyPlayerHandler.SetYPos(yPos);
     }
 }
