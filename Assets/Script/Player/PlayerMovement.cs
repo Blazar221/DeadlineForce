43571cf70f9de3a6937004ed79a0303bdc5bb568
Wentao Chen <68458163+BattleManCWT@users.noreply.github.com>
2022-11-03 23:25:05 -0700

    update playerMovement

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool canChangeGravity;
+    private Animator animator;
+    private Rigidbody2D rb2D;
+    private bool isUpsideDown;
+    private bool reverseControl = false;
+    private float[] playerYPosArr;
+    private int curYPos;
+
     void Awake()
     {
-        
+        canChangeGravity = true;
+
+        playerYPosArr = new float[4];
+        playerYPosArr[0] = 3.4f;
+        playerYPosArr[1] = 1.6f;
+        playerYPosArr[2] = -1.6f;
+        playerYPosArr[3] = -3.4f;
+        curYPos = 1;
+        isUpsideDown = false;
+
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 curPosition = transform.position;
         
+        if (canChangeGravity)
+		{
+            if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                if(!reverseControl)
+                {
+                    SetYPos((curYPos+3)%4);
+                }
+                else
+                {
+                    SetYPos((curYPos+1)%4);
+                }
+            }
+            if((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
+            {
+                if(!reverseControl)
+                {
+                    SetYPos((curYPos+1)%4);
+                }
+                else
+                {
+                    SetYPos((curYPos+3)%4);
+                }
+            }
+		}
+    }
+
+    public void SetYPos(int yPos)
+    {
+        curYPos = yPos;
+        // Directly move position
+        transform.position = new Vector3(transform.position.x, playerYPosArr[yPos], transform.position.z);
+        // Set Gravity Direction and isUpsideDown Flag
+        if(yPos == 0 || yPos == 2)
+        {
+            rb2D.gravityScale = -1f;
+            isUpsideDown = true;
+        }
+        else
+        {
+            rb2D.gravityScale = 1f;
+            isUpsideDown = false;
+        }
+        canChangeGravity = false;
+        animator.SetBool("UpsideDown",isUpsideDown);
     }
 }
