a197bdedcc4fb8e8729f7ce2250d299a4edbf049
Blazar <805781038@qq.com>
2022-11-25 00:36:55 -0800

    big red logic finish

--- Assets/Script/Boss/BossBehavior.cs
+++ Assets/Script/Boss/BossBehavior.cs
@@ -14,18 +14,24 @@ public class BossBehavior : MonoBehaviour
 
     [SerializeField] private PlayerMovement playerMovement;
 
-    [SerializeField] private float bossMoveSpeed = 0.3f;
+    [SerializeField] private float bossMoveSpeed = 1f;
     [SerializeField] private float bossAttackPeriod = 5f;
 
     [SerializeField] private GameObject laser;
     [SerializeField] private GameObject bandit;
     public int laserHarm=20, banditHarm=10;
     
-    private int _attackingLine;
     private int _count=0;
-    private float _moveDestY;
+    
+    private int attackingLine;
+    private float moveDestY;
     private Vector3 moveDest;
-    public bool startMove = false;
+
+    bool moving = false;
+    bool attackWaiting = false;
+    bool retreatWaiting = false;
+
+    bool canHurtPlayer = true;
 
     private Vector3 originalLocalScale;
 
@@ -39,12 +45,31 @@ public class BossBehavior : MonoBehaviour
     private BgmController _bgmHandler;
     private bool _startStruggle = false;
 
+    private Vector3 playerOffset = new Vector3(2f, 0f, 0f);
+    private float playerX = -2f;
+    private float bossX = 5f;
+
+    private float[] bigRedYArr = {3.77f, 1.81f, -1.55f, -3.53f};
+    private float[] currentYArr;
+
+    private Color freezeColor = new Color(0f, 0.9f, 0.9f, 1f);
+
     void Awake()
     {   
         Instance = this;
         bossAnimator = GetComponent<Animator>();
+
         _bossUI = GetComponent<BossUI>();
         _bgmHandler = bgm.GetComponent<BgmController>();
+
+        switch(name)
+        {
+            case "BigRed":
+                currentYArr = bigRedYArr;
+                break;
+            default:
+                break;
+        }
 
         originalLocalScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
@@ -59,64 +84,55 @@ public class BossBehavior : MonoBehaviour
         Alert2.GetComponent<Animator>().enabled = false;
         Alert3.GetComponent<SpriteRenderer>().enabled = false;
         Alert3.GetComponent<Animator>().enabled = false;
-        StartCoroutine(AutoAttack());
+
+        switch(name){
+            case "BigRed":
+                StartCoroutine(BigRedAutoAttack());
+                break;
+            default:
+                break;
+        }
+        // StartCoroutine(AutoAttack());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(startMove)
+        if(moving)
         {
             CheckMoveEnd();
         }
     }
-    
-    // Line Index from top to bottom: 0, 1, 2, 3
-    public IEnumerator AutoAttack()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(bossAttackPeriod);
 
-            _attackingLine = playerMovement.GetYPos();
-            _moveDestY = _attackingLine switch
-            {
-                0 => 4.92f,
-                1 => 0.57f,
-                2 => -0.57f,
-                3 => -4.92f,
-                _ => 0,
-            };
-
-            if(_attackingLine == 0 || _attackingLine == 2)
-            {
-                transform.localScale = new Vector3(originalLocalScale.x, -originalLocalScale.y, originalLocalScale.z);
-            }
-            else
-            {
-                transform.localScale = new Vector3(originalLocalScale.x, originalLocalScale.y, originalLocalScale.z);
-            }
-
-            moveDest = new Vector3(transform.position.x, _moveDestY, transform.position.z);
-            startMove = true;
-            bossAnimator.SetBool("isMove", true);
-            
-            if (_startStruggle == false && _bgmHandler.songPosition > struggleStartTime)
-            {
-                Struggle();
-                _bossUI.StruggleColor();
-                _startStruggle = true;
-            }
-        }
-    }
-    
     public void CheckMoveEnd()
     {
         transform.position = Vector3.MoveTowards(transform.position, moveDest, bossMoveSpeed);
-        if(transform.position.y == _moveDestY){
-            startMove = false;
+        if(transform.position == moveDest){
+            moving = false;
             bossAnimator.SetBool("isMove", false);
-            bossAnimator.SetTrigger("attack");
+            if(attackWaiting)
+            {  
+                attackWaiting = false;
+                bossAnimator.SetTrigger("attack");
+                if(canHurtPlayer){
+                    PlayerHealth.Instance.TakeDamage(20);
+                }
+            }
+            if(retreatWaiting){
+                StartCoroutine(CallRetreat());
+            }
+        }
+    }
+
+    IEnumerator CallRetreat(){
+        switch(name)
+        {
+            case "BigRed":
+                yield return new WaitForSeconds(1f);
+                CallBigRedRetreat();
+                break;
+            default:
+                break;
         }
     }
 
@@ -124,7 +140,7 @@ public class BossBehavior : MonoBehaviour
     {
         bossMoveSpeed /= 10;
         bossAttackPeriod *= 2;
-        BossUI.Instance.SetColor(Color.blue);
+        BossUI.Instance.SetColor(freezeColor);
         StartCoroutine(Unfreeze());
     }
 
@@ -135,11 +151,119 @@ public class BossBehavior : MonoBehaviour
         bossAttackPeriod /=2;
         BossUI.Instance.SetColor(Color.white);
     }
-    
-    public void Attack()
-    {
-        StartCoroutine(SpawnAttack());
+
+    void SetLocalScale(){
+        if(attackingLine == 0 || attackingLine == 2)
+        {
+            transform.localScale = new Vector3(originalLocalScale.x, -originalLocalScale.y, originalLocalScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(originalLocalScale.x, originalLocalScale.y, originalLocalScale.z);
+        }
     }
+
+    /***********************************
+    **          Boss: BigRed          **
+    ************************************/
+    IEnumerator BigRedAutoAttack()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(bossAttackPeriod);
+            CallBigRedTrack();
+        }
+    }
+
+    void CallBigRedTrack()
+    {
+        attackingLine = playerMovement.GetYPos();
+        moveDestY = currentYArr[attackingLine];
+        moveDest = new Vector3(playerX, moveDestY, 0);
+
+        SetLocalScale();
+
+        moving = true;
+        attackWaiting = true;
+        retreatWaiting = true;
+
+        bossAnimator.SetBool("isMove", true);
+    }
+
+    void CallBigRedRetreat()
+    {
+        attackingLine = playerMovement.GetYPos();
+        moveDestY = currentYArr[attackingLine];
+        moveDest = new Vector3(bossX, moveDestY, 0);
+
+        SetLocalScale();
+
+        moving = true;
+        attackWaiting = false;
+        retreatWaiting = false;
+
+        bossAnimator.SetBool("isMove", true);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.name == "Player")
+        {
+            canHurtPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.name == "Player")
+        {
+            canHurtPlayer = false;
+        }
+    }
+
+    
+    // // Line Index from top to bottom: 0, 1, 2, 3
+    // public IEnumerator AutoAttack()
+    // {
+
+    //     while (true)
+    //     {
+    //         yield return new WaitForSeconds(bossAttackPeriod);
+
+    //         attackingLine = playerMovement.GetYPos();
+    //         moveDestY = currentYArr[attackingLine];
+
+    //         if(attackingLine == 0 || attackingLine == 2)
+    //         {
+    //             transform.localScale = new Vector3(originalLocalScale.x, -originalLocalScale.y, originalLocalScale.z);
+    //         }
+    //         else
+    //         {
+    //             transform.localScale = new Vector3(originalLocalScale.x, originalLocalScale.y, originalLocalScale.z);
+    //         }
+
+    //         moveDest = new Vector3(transform.position.x, moveDestY, transform.position.z);
+    //         moving = true;
+    //         bossAnimator.SetBool("isMove", true);
+            
+    //         if (_startStruggle == false && _bgmHandler.songPosition > struggleStartTime)
+    //         {
+    //             Struggle();
+    //             _bossUI.StruggleColor();
+    //             _startStruggle = true;
+    //         }
+    //     }
+    // }
+    
+    // public void Attack()
+    // {
+    //     switch(name){
+    //         case "BigRed":
+    //             CallBigRedAttack();
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     // StartCoroutine(SpawnAttack());
+    // }
     
     public void StartAlert(int pos)
     {
@@ -190,64 +314,64 @@ public class BossBehavior : MonoBehaviour
                 break;
         }
     }
-    
-    IEnumerator SpawnAttack()
-    {
-        float pos1 = LineIndToPos(_attackingLine),
-            pos2 = LineIndToPos((_attackingLine + 3) % 4),
-            pos3 = LineIndToPos((_attackingLine + 1) % 4);
-        switch (_count)
-        {
-            case 0:
-                StartAlert(_attackingLine);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(laser, new Vector3(-4, pos1, 0), Quaternion.identity);
-                Destroy(_newBullet, 1f);
-                EndAlert(_attackingLine);
+
+    // IEnumerator SpawnAttack()
+    // {
+    //     float pos1 = LineIndToPos(attackingLine),
+    //         pos2 = LineIndToPos((attackingLine + 3) % 4),
+    //         pos3 = LineIndToPos((attackingLine + 1) % 4);
+    //     switch (_count)
+    //     {
+    //         case 0:
+    //             StartAlert(attackingLine);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(laser, new Vector3(-4, pos1, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 1f);
+    //             EndAlert(attackingLine);
                 
-                yield return new WaitForSeconds(0.3f);
+    //             yield return new WaitForSeconds(0.3f);
                 
-                StartAlert((_attackingLine + 3) % 4);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(bandit, new Vector3(6, pos2, 0), Quaternion.identity);
-                Destroy(_newBullet, 2f);
-                EndAlert((_attackingLine + 3) % 4);
+    //             StartAlert((attackingLine + 3) % 4);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(bandit, new Vector3(6, pos2, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 2f);
+    //             EndAlert((attackingLine + 3) % 4);
                 
-                yield return new WaitForSeconds(0.3f);
+    //             yield return new WaitForSeconds(0.3f);
                 
-                StartAlert((_attackingLine + 1) % 4);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(bandit, new Vector3(6, pos3, 0), Quaternion.identity);
-                Destroy(_newBullet, 2f);
-                EndAlert((_attackingLine + 1) % 4);
-                break;
-            default:
-                StartAlert(_attackingLine);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(bandit, new Vector3(6, pos1, 0), Quaternion.identity);
-                Destroy(_newBullet, 2f);
-                EndAlert(_attackingLine);
+    //             StartAlert((attackingLine + 1) % 4);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(bandit, new Vector3(6, pos3, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 2f);
+    //             EndAlert((attackingLine + 1) % 4);
+    //             break;
+    //         default:
+    //             StartAlert(attackingLine);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(bandit, new Vector3(6, pos1, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 2f);
+    //             EndAlert(attackingLine);
                 
-                yield return new WaitForSeconds(0.3f);
+    //             yield return new WaitForSeconds(0.3f);
                 
-                StartAlert((_attackingLine + 3) % 4);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(bandit, new Vector3(6, pos2, 0), Quaternion.identity);
-                Destroy(_newBullet, 2f);
-                EndAlert((_attackingLine + 3) % 4);
+    //             StartAlert((attackingLine + 3) % 4);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(bandit, new Vector3(6, pos2, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 2f);
+    //             EndAlert((attackingLine + 3) % 4);
                 
-                yield return new WaitForSeconds(0.3f);
+    //             yield return new WaitForSeconds(0.3f);
                 
-                StartAlert((_attackingLine + 1) % 4);
-                yield return new WaitForSeconds(1.2f);
-                _newBullet = Instantiate(bandit, new Vector3(6, pos3, 0), Quaternion.identity);
-                Destroy(_newBullet, 2f);
-                EndAlert((_attackingLine + 1) % 4);
-                break;
-                ;
-        }
-        _count = (_count + 1)%3;
-    }
+    //             StartAlert((attackingLine + 1) % 4);
+    //             yield return new WaitForSeconds(1.2f);
+    //             _newBullet = Instantiate(bandit, new Vector3(6, pos3, 0), Quaternion.identity);
+    //             Destroy(_newBullet, 2f);
+    //             EndAlert((attackingLine + 1) % 4);
+    //             break;
+    //             ;
+    //     }
+    //     _count = (_count + 1)%3;
+    // }
 
     public void Struggle()
     {
